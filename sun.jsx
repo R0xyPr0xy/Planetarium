7a74b209bbc65380566d8c4fcd781c2101fa10ad
R0xyPr0xy <pinkinkyquill@mail.com>
2025-02-09 23:19:16 +1300

    Added Sun page, right excerpts and bloom pass

--- sun.jsx
+++ sun.jsx
@@ -2,11 +2,14 @@ import * as THREE from 'three';
 // import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let planetName = "Earth";
-let stinger = "Third planet from the Sun";
+let planetName = "Sun";
+let stinger = "The center of our system";
 
 let planetNameBox = document.querySelector(".planet-name");
 planetNameBox.innerHTML += planetName;
@@ -16,13 +19,13 @@ stingerBox.innerHTML += stinger;
 
 // Fetch temperature
 async function fetchTemperature() {
-  let url = "https://www.wikidata.org/wiki/Special:EntityData/Q2.json";
+  let url = "https://www.wikidata.org/wiki/Special:EntityData/Q525.json";
 
   let response = await fetch(url);
   let data = await response.json();
 
   // Extracting temperature
-  let temperature = data.entities.Q2.claims.P2076[0].mainsnak.datavalue.value.amount;
+  let temperature = data.entities.Q525.claims.P2076[0].mainsnak.datavalue.value.amount;
   let msg = `Temperature : <span class="highlighted">${temperature} kelvin </span>`;
 
   let tempBox = document.querySelector(".temperature");
@@ -33,13 +36,13 @@ fetchTemperature();
 
 // Fetch radius
 async function fetchRadius() {
-  let url = "https://www.wikidata.org/wiki/Special:EntityData/Q2.json";
+  let url = "https://www.wikidata.org/wiki/Special:EntityData/Q525.json";
 
   let response = await fetch(url);
   let data = await response.json();
 
   // Extracting radius
-  let radius = data.entities.Q2.claims.P2120[0].mainsnak.datavalue.value.amount;
+  let radius = data.entities.Q525.claims.P2076[0].mainsnak.datavalue.value.amount;
   let msg = `Radius : <span class="highlighted">${radius} km </span>`;
 
   let radiusBox = document.querySelector(".radius");
@@ -49,7 +52,7 @@ async function fetchRadius() {
 fetchRadius();
 
 // Fetch wiki excerpt
-let pageTitle = "Earth";
+let pageTitle = "Sun";
 let url = "https://en.wikipedia.org/api/rest_v1/page/summary/" + pageTitle;
 
 async function fetchExcerpt() {
@@ -66,12 +69,25 @@ fetchExcerpt()
 const scene = new THREE.Scene();
 var renderWidth = window.innerWidth * (45 / 100); 
 const camera = new THREE.PerspectiveCamera( 75, renderWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 1200;
+camera.position.z = 25;
 
 // Set up renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( renderWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+// Add renderpass
+const renderScene = new RenderPass(scene, camera);
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+
+const bloomPass = new UnrealBloomPass(
+    new THREE.Vector2(renderWidth, window.innerHeight),
+    1.2,
+    0.1,
+    0.1
+);
+composer.addPass(bloomPass);
 
 // Set up light
 const light = new THREE.DirectionalLight(0xffffff, 2); 
@@ -82,14 +98,14 @@ scene.add(light);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-let earthModel;
+let sunModel;
 
 // create loader
 const loader = new GLTFLoader();
-loader.load('/3D-assets/earth.glb', function(gltf) {
-  earthModel = gltf.scene;
-  scene.add(earthModel);
-  earthModel.rotation.set(0,330,0);
+loader.load('/3D-assets/sun.glb', function(gltf) {
+  sunModel = gltf.scene;
+  sunModel.emissive
+  scene.add(sunModel);
 
   animate(); // Animate only once model is loaded
 
@@ -97,14 +113,15 @@ loader.load('/3D-assets/earth.glb', function(gltf) {
   console.error('Error loading GLTF model:', error);
 });
 
-// Animate earth model
+// Animate sun model
 function animate() {
-  if (earthModel) {
-    earthModel.rotation.x += 0.0002;
-    earthModel.rotation.y += 0.0002;
+  if (sunModel) {
+    sunModel.rotation.x += 0.0002;
+    sunModel.rotation.y += 0.0002;
   }
 
-  renderer.render( scene, camera );
+//   renderer.render( scene, camera );
+composer.render();
   requestAnimationFrame(animate);
 }
 animate();
