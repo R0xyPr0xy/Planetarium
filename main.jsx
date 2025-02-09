08f8fc4987c93f706ac66d08760ede4fdc24f665
R0xyPr0xy <pinkinkyquill@mail.com>
2025-02-09 18:18:54 +1300

    Loaded mercury model and animation

--- main.jsx
+++ main.jsx
@@ -1,91 +1,53 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+// Set up scene and camera
+const scene = new THREE.Scene();
+var renderWidth = window.innerWidth * (45 / 100); 
+const camera = new THREE.PerspectiveCamera( 75, renderWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 1200;
 
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+// Set up renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( renderWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// Set up light
+const light = new THREE.DirectionalLight(0xffffff, 2); 
+light.position.set(10, 10, 10);
+scene.add(light);
+
+// Set up orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+let mercuryModel;
+
+// create loader
+const loader = new GLTFLoader();
+loader.load('/3D-assets/mercury.glb', function(gltf) {
+  mercuryModel = gltf.scene;
+  scene.add(mercuryModel);
+  animate(); // Animate only once model is loaded
+
+}, undefined, function (error) {
+  console.error('Error loading GLTF model:', error);
+});
+
+// Animate mercury model
+function animate() {
+  if (mercuryModel) {
+  mercuryModel.rotation.x += 0.0002;
+  mercuryModel.rotation.y += 0.0002;
+  }
+
+  renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+}
+animate();
 
 
-// // ---------------------------------------------- Cube example ----------------------------------------------------
-
-// // Set up scene and camera
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-// camera.position.z = 3;
-
-// // Set up renderer
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// // Create a cube
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x753355 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-// // Animate cube
-// function animate() {
-//     cube.rotation.x += 0.01;
-//     cube.rotation.y += 0.01;
-// 	renderer.render( scene, camera );
-// }
-// renderer.setAnimationLoop( animate );
-
-// ---------------------------------------------- Line example ----------------------------------------------------
-// // Set up renderer
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// //Set up camera
-// const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-// camera.position.set( 0, 0, 100 );
-// camera.lookAt( 0, 0, 0 );
-
-// const scene = new THREE.Scene();
-
-// // Create a blue LineBasicMaterial
-// const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-// // Define line points
-// const points = [];
-// points.push( new THREE.Vector3( - 10, 0, 0 ) );
-// points.push( new THREE.Vector3( -5, 10, 0 ) );
-// points.push( new THREE.Vector3( 0, 15, 0 ) );
-// points.push( new THREE.Vector3( 5, 10, 0 ) );
-// points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-// const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-// const line = new THREE.Line( geometry, material );
-
-// scene.add( line );
-// renderer.render( scene, camera );
-
-// ---------------------------------------------- Imported object example ----------------------------------------------------
-
-// Instantiate a loader
-// const loader = new GLTFLoader().setPath( 'public/spiked_ball/' );
-// 	loader.load( 'scene.gltf', async function ( gltf ) {
-
-// 		const model = gltf.scene;
-
-// 		// wait until the model can be added to the scene without blocking due to shader compilation
-
-// 		await renderer.compileAsync( model, camera, scene );
-
-// 		scene.add( model );
-
-// 		render();
-			
-// 		} );
-
-// function render() {
-//     renderer.render( scene, camera );   
-// }
